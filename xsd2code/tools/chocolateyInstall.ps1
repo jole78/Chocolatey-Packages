aec32e9b203acd1b7dcaac3e3224596019fa3383
Johan Leino <johan.leino@hm.com>
2015-06-15 08:27:06 +0200

    added xsd2code 3.4.0

---
+++ xsd2code/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'Xsd2Code'
+$fileType = 'msi'
+$url = 'https://xsd2code.codeplex.com/downloads/get/830294'
+$silentArgs = '/quiet'
+
+Install-ChocolateyPackage $packageName $fileType "$silentArgs" "$url"
